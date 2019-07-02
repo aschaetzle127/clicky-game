52aa6f967ef8de8317b666ea9d564b2564d213ab
Lexi127 <aperalta127@gmail.com>
2019-07-01 18:20:33 -0700

    Updates

---
+++ precache-manifest.305f2aca72505e6ca35f5e42cfeccbe8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6af163ddae893a4bc32ea0b9fe6bb029",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "f4efc65d1fa0596eb3a4",
+    "url": "/clicky-game/static/css/main.d1cea68f.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/clicky-game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "f4efc65d1fa0596eb3a4",
+    "url": "/clicky-game/static/js/main.f9fe203f.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  },
+  {
+    "revision": "d81d8166455ef5fc2617db81158031f8",
+    "url": "/clicky-game/static/media/earth.d81d8166.jpeg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/clicky-game/precache-manifest.211f3c4a8eca55ab3b7dabc38a4e5ae1.js"
+  "/clicky-game/precache-manifest.305f2aca72505e6ca35f5e42cfeccbe8.js"
 );
 
 self.addEventListener('message', (event) => {
